a782ce92f8c999221f88a6061dc72731015e08bf
Timothy Yu <sun.spring.noodle@gmail.com>
2017-05-02 13:53:50 +0100

    Remove dependency

--- build/generated/source/r/debug/com/spotify/sdk/android/authentication/R.java
+++ build/generated/source/r/debug/com/spotify/sdk/android/authentication/R.java
@@ -8,16 +8,16 @@ package com.spotify.sdk.android.authentication;
 
 public final class R {
     public static final class id {
-        public static final int com_spotify_sdk_login_webview = 0x7f06000f;
-        public static final int com_spotify_sdk_login_webview_container = 0x7f06000e;
+        public static final int com_spotify_sdk_login_webview = 0x7f09000f;
+        public static final int com_spotify_sdk_login_webview_container = 0x7f09000e;
     }
     public static final class layout {
-        public static final int com_spotify_sdk_login_activity = 0x7f030002;
-        public static final int com_spotify_sdk_login_dialog = 0x7f030003;
+        public static final int com_spotify_sdk_login_activity = 0x7f040002;
+        public static final int com_spotify_sdk_login_dialog = 0x7f040003;
     }
     public static final class string {
-        public static final int com_spotify_sdk_login_progress = 0x7f050001;
-        public static final int com_spotify_sdk_redirect_host = 0x7f050002;
-        public static final int com_spotify_sdk_redirect_scheme = 0x7f050003;
+        public static final int com_spotify_sdk_login_progress = 0x7f080007;
+        public static final int com_spotify_sdk_redirect_host = 0x7f080008;
+        public static final int com_spotify_sdk_redirect_scheme = 0x7f080009;
     }
 }
