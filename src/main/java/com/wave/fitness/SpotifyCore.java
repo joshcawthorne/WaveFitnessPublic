3a06d366f08d5d71bb8d708d8eea75807e5c8fa6
Timothy Yu <sun.spring.noodle@gmail.com>
2017-05-08 17:15:48 +0100

    Update music player

---
+++ src/main/java/com/wave/fitness/SpotifyCore.java
@@ -0,0 +1,20 @@
+package com.wave.fitness;
+
+import android.app.Application;
+
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.Metadata;
+import com.spotify.sdk.android.player.PlaybackState;
+import com.spotify.sdk.android.player.SpotifyPlayer;
+
+/**
+ * Created by s6236422 on 08/05/2017.
+ */
+
+public class SpotifyCore extends Application {
+    public SpotifyPlayer mPlayer;
+    public PlaybackState mCurrentPlaybackState;
+    public Metadata mMetadata;
+    public boolean isLoggedIn = false;
+    public AuthenticationResponse authResponse;
+}
