65225606d154d0ef45c165c87cd95158f73fa6bc
Josh Cawthorne <joshcawthorne97@gmail.com>
2017-05-17 22:29:35 +0100

    Remove drawables.

--- src/main/java/com/wave/fitness/PostRunActivity.java
+++ src/main/java/com/wave/fitness/PostRunActivity.java
@@ -86,8 +86,6 @@ public class PostRunActivity extends AppCompatActivity implements OnMapReadyCall
     @InjectView(R.id.metaDataTimeFive) TextView _time5;
 
 
-
-
     private HamburgerMenu menu;
     Toolbar toolbar;
 
